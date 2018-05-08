6929b7b70aad2e500c3873060c79dafff9d4e15a
messiaslima <messiaslima.03@gmail.com>
2018-05-08 20:45:51 -0300

    peca page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,36 +5,36 @@ import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { PecaPage } from '../pages/peca/peca';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+	declarations: [
+		MyApp,
+		AboutPage,
+		ContactPage,
+		PecaPage,
+		TabsPage
+	],
+	imports: [
+		BrowserModule,
+		IonicModule.forRoot(MyApp)
+	],
+	bootstrap: [IonicApp],
+	entryComponents: [
+		MyApp,
+		AboutPage,
+		ContactPage,
+		PecaPage,
+		TabsPage
+	],
+	providers: [
+		StatusBar,
+		SplashScreen,
+		{ provide: ErrorHandler, useClass: IonicErrorHandler }
+	]
 })
-export class AppModule {}
+export class AppModule { }
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { PecaPage } from '../peca/peca';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
+  tab1Root = PecaPage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
 
