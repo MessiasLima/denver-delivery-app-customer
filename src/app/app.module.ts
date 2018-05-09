c1f9fb9e4103d50ae01699b719b60408a4014cf5
messiaslima <messiaslima.03@gmail.com>
2018-05-08 21:20:00 -0300

    build android

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 	],
 	imports: [
 		BrowserModule,
-		IonicModule.forRoot(MyApp)
+		IonicModule.forRoot(MyApp,{
+			mode: "ios"
+		})
 	],
 	bootstrap: [IonicApp],
 	entryComponents: [
