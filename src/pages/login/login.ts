df55569fe13a8b7728aac89b23dc930891075e13
messias <messiaslima.03@gmail.com>
2018-05-25 20:34:49 -0300

    teste login facebook

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -13,9 +13,7 @@ export class LoginPage {
 		public navCtrl: NavController,
 		public navParams: NavParams,
 		public facebook: Facebook
-	) {
-	}
-
+	) { }
 
 	//método para chamar api do facebook e salvar no banco o usuario    
 	loginFacebook() {
@@ -23,22 +21,34 @@ export class LoginPage {
 		permissions = ["public_profile", "email"];
 
 		this.facebook.login(permissions).then((response) => {
-			
+
 			console.log("response login");
 			console.log(response);
-		
-			let params = new Array<string>();
-			this.facebook.api("/me?fields=name,email", params)
-				.then(res => {
-					console.log("response api");
-					console.log(res);
-				}, (error) => {
-					alert(error);
-					console.log('ERRO LOGIN: ', error);
-				});
+
+
 
 		}, (error) => {
 			alert(error);
 		});
+	}
+
+	apiFacebook() {
+		let params = new Array<string>();
+		this.facebook.api("/me?fields=name,email", params)
+			.then(res => {
+				console.log("response api");
+				console.log(res);
+			}, (error) => {
+				alert(error);
+				console.log('ERRO LOGIN: ', error);
+			});
+	}
+
+	logoutFacebook(){
+		this.facebook.logout().then(
+			data =>{
+				console.log(data);
+			}
+		)
 	}
 }
